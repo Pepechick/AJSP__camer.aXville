75af66664aa542a84c490352e062ed58fde0270a
Sismo44 <Simonloison2@gmail.com>
2026-01-23 20:52:37 +0100

    nouvelles modifs

---
+++ get_camera.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+
+$pdo = new PDO("mysql:host=localhost;dbname=Camera;charset=utf8", "root", "ChuckNorris44",[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+$coo = "SELECT id_cam, latitude, longitude, ville, rue, utilisateurs_id FROM cameras";
+$stmt = $pdo->query($coo);
+
+echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+?>
